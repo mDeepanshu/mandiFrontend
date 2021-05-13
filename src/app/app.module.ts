5f5bb0807aecfb000391ec791e42a6e410873519
mDeepanshu <deepanshumalviya8964@gmail.com>
2021-05-13 08:39:19 -0700

    routing setup completed

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -15,12 +15,14 @@ import { HttpClientModule } from '@angular/common/http';
 import { PartyCollectionComponent } from './party-collection/party-collection.component';
 const routes: Routes = [
   { path: '', component: PurchaseComponent },
+  { path: 'partyCollection-component', component: PartyCollectionComponent },
   { path: 'mainMenu-component', component: MainMenuComponent },
   { path: 'viewPurchase-component', component: ViewPurchaseComponent },
   { path: 'ledger-component', component: LedgerComponent },
   { path: 'viewParty-component', component: ViewPartyComponent },
   { path: 'login-component', component: LoginComponent },
   { path: 'crate-component', component: CrateComponent },
+  { path: 'addParty-component', component: AddPartyComponent },
 ];
 
 @NgModule({
