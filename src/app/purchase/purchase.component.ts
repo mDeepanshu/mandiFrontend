63b0003825d0f867a45174cde0ca4dae5ef7904f
mDeepanshu <deepanshumalviya8964@gmail.com>
2021-05-21 05:28:58 +0530

    crate functionality adding

--- src/app/purchase/purchase.component.ts
+++ src/app/purchase/purchase.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { MainServiceService } from '../main-service.service';
+import { FormControl, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-purchase',
@@ -9,7 +10,30 @@ import { MainServiceService } from '../main-service.service';
 export class PurchaseComponent implements OnInit {
   constructor(private mainService: MainServiceService) {}
 
-  ngOnInit(): void {}
+  purchaseForm: FormGroup;
+  public tableArr: Array<any> = [];
+  ngOnInit() {
+    this.purchaseForm = new FormGroup({
+      setOne: new FormGroup({
+        bhada: new FormControl(null),
+        hammali: new FormControl(null),
+        cash: new FormControl(null),
+        commRate: new FormControl(null),
+        bhadaRate: new FormControl(null),
+        comm: new FormControl(null),
+        tax: new FormControl(null),
+        stationCharge: new FormControl(null),
+        driver: new FormControl(null),
+      }),
+      setTwo: new FormGroup({
+        item: new FormControl(null),
+        bag: new FormControl(null),
+        quantity: new FormControl(null),
+        rate: new FormControl(null),
+        amount: new FormControl(null),
+      }),
+    });
+  }
   public timer;
 
   partyName(val) {
@@ -18,5 +42,12 @@ export class PurchaseComponent implements OnInit {
       console.log(val);
       this.mainService.autoCompleteName(val, '', '');
     }, 1000);
+  }
+  addNew() {
+    console.log(this.purchaseForm.value.setTwo);
+    this.tableArr.push(this.purchaseForm.value.setTwo);
+  }
+  onSubmit() {
+    console.log(this.purchaseForm.value);
   }
 }
--- src/app/sell/sell.component.ts
+++ src/app/sell/sell.component.ts
@@ -1,15 +1,34 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup } from '@angular/forms';
+import { MainServiceService } from '../main-service.service';
 
 @Component({
   selector: 'app-sell',
   templateUrl: './sell.component.html',
-  styleUrls: ['./sell.component.css']
+  styleUrls: ['./sell.component.css'],
 })
 export class SellComponent implements OnInit {
+  constructor(private mainService: MainServiceService) {}
+  public tableArr: Array<any> = [];
+  sellForm: FormGroup;
+  public timer;
 
-  constructor() { }
-
-  ngOnInit(): void {
+  ngOnInit() {
+    this.sellForm = new FormGroup({
+      partyName: new FormControl(null),
+      amount: new FormControl(null),
+    });
+  }
+  addNew() {
+    console.log(this.sellForm.value);
+    this.tableArr.push(this.sellForm.value);
   }
 
+  itemName(val) {
+    clearTimeout(this.timer);
+    this.timer = setTimeout(() => {
+      console.log(val);
+      // this.mainService.autoCompleteName(val, '', '');
+    }, 1000);
+  }
 }
