23fb0dd949eab8f276fdf5793483f895069188c4
mDeepanshu <deepanshumalviya8964@gmail.com>
2021-05-22 01:49:36 +0530

    7 new func added in shared service

--- src/app/main-service.service.ts
+++ src/app/main-service.service.ts
@@ -39,4 +39,46 @@ export class MainServiceService {
       console.log(res);
     });
   }
+  checkNameavail(val) {
+    this.http
+      .get(`${this.url}/party/check_availability?name=${val}`)
+      .subscribe((res) => {
+        console.log(res);
+      });
+  }
+  editPartyName(val) {
+    this.http
+      .get(`${this.url}/party/edit_party?partyId=${val}`)
+      .subscribe((res) => {
+        console.log(res);
+      });
+  }
+  addNewPurchase(body) {
+    this.http.post(`${this.url}/purchase/add_new`, body).subscribe((res) => {
+      console.log(res);
+    });
+  }
+  getBillbyNum(val) {
+    this.http
+      .get(`${this.url}/purchase/by_bill_no?bill_no=${val}`)
+      .subscribe((res) => {
+        console.log(res);
+      });
+  }
+  getBillbyDate(toDate, fromDate) {
+    this.http
+      .get(
+        `${this.url}/purchase/by_date?to_date=${toDate}&from_date=${fromDate}`
+      )
+      .subscribe((res) => {
+        console.log(res);
+      });
+  }
+  getBillbyParty(limit, page) {
+    this.http
+      .get(`${this.url}/purchase/by_party?limit=${limit}&page=${page}`)
+      .subscribe((res) => {
+        console.log(res);
+      });
+  }
 }
